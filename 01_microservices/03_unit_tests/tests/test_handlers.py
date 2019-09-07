9962e0f0a5308684a68df576e525bbb1a22198fb
daniel.barragan <daniel.barragan@alertlogic.com>
2019-09-07 10:21:58 -0500

    Add files

--- 01_microservices/03_unit_tests/tests/test_handlers.py
+++ 01_microservices/03_unit_tests/tests/test_handlers.py
@@ -10,6 +10,10 @@ def client():
     with flask_app.app.test_client() as c:
         yield c
 
+@pytest.fixture()
+def user_role():
+    return 'collaborator'
+
 def test_get_health(client):
     # GIVEN no query parameters or payload
     # WHEN I access to the url GET /health
@@ -17,11 +21,23 @@ def test_get_health(client):
     response = client.get('/health')
     assert response.status_code == 404
 
-def test_get_user_info(mocker, user_info):
+def test_get_user_info():
     # GIVEN not query parameters or payload
     # WHEN I access to the url GET /users/john.doe
     # THEN the information for the user must be returned
-    mocker.patch.object(handlers, 'get_user_info', return_value=user_info)
     user_info = handlers.get_user_info('john.doe')
-    assert {'username': 'john.doe', 'id': '123', 'role': 'admin'} == user_info
+    assert user_info['username'] == 'john.doe'
+    assert user_info['id'] == '123'
+    assert user_info['role'] == 'admin'
+    assert len(user_info) == 3
 
+def test_get_user_info_with_patch(mocker, user_info):
+    # GIVEN not query parameters or payload
+    # WHEN I access to the url GET /users/john.doe
+    # THEN the information for the user must be returned
+    mocker.patch.object(handlers, 'query_role', return_value=user_role)
+    user_info = handlers.get_user_info('john.doe')
+    assert user_info['username'] == 'john.doe'
+    assert user_info['id'] == '123'
+    assert user_info['role'] == 'collaborator'
+    assert len(user_info) == 3
